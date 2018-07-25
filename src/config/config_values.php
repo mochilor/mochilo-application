a8cf56f47f2d2dc7eb2432c5e982010471ed6bfe
Diego Altamirano Rodríguez <diego.altamirano.rodriguez@gmail.com>
2018-07-25 22:59:19 +0200

    Minor config fix

--- src/config/config_values.php
+++ src/config/config_values.php
@@ -2,7 +2,14 @@
 
 return [
     'debug' => filter_var(getenv('DEBUG'), FILTER_VALIDATE_BOOLEAN),
-    'example' => [
-        'key' => 'value',
+    'mail' => [
+        'address' => filter_var(getenv('MAIL_USERNAME')),
+        'username' => filter_var(getenv('MAIL_USERNAME')),
+        'password' => filter_var(getenv('MAIL_PASSWORD')),
+        'host' => filter_var(getenv('MAIL_HOST')),
+        'port' => filter_var(getenv('MAIL_PORT')),
+        'smtp_auth' => filter_var(getenv('MAIL_AUTH'), FILTER_VALIDATE_BOOLEAN),
+        'smtp_secure' => filter_var(getenv('MAIL_SECURE')),
+        'name' => 'My name',
     ],
 ];
